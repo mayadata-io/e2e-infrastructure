3129fdadafd799f22b98fa3648489acfb2672fb6
harshvkarn <harshvkarn54@gmail.com>
2018-10-11 16:28:16 +0530

    Add licence
    Signed-off-by: harshvkarn <harshvkarn54@gmail.com>

--- env/env_exporter.py
+++ env/env_exporter.py
@@ -1,5 +1,24 @@
-# HOW TO RUN:
-# export TEST=$(eval python env_exporter.py -o mapi)
+# 
+# Copyright 2018 The OpenEBS Authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+#
+# Info:
+#This Python script can be used to generate the image name with tag
+# HOW TO EXPORT:
+# $ export OPENEBS_IO_JIVA_CONTROLLER_IMAGE=$(eval python env_exporter.py -o jcontroller)
+# -o, --option: jcontroller, jreplica, mapi and iovolume
+
 import os
 import argparse
 
@@ -71,10 +90,6 @@ def run(option):
     else:
         print 'Invalid Entry!!'
         return
-    # print maya
-    # print jiva
-    # print cstor
-    # print option
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='returns')
     parser.add_argument('-o','--option', help='Enter the repo name', required=True)
